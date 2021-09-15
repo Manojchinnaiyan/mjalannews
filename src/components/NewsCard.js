5f44de0e4117b652881dcb2724cfc35b5a86782d
Manoj Chinnaiyan <52351780+Manojchinnaiyan@users.noreply.github.com>
2021-09-15 17:43:38 +0530

    changes

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Grid, Grow, Typography } from "@material-ui/core"
+import { Grid, Grow } from "@material-ui/core"
 import useStyles from "./NewsCardStyles"
 import Banner from "./Banner"
 
